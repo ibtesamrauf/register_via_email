7a56fb9dc4a2c71dbb29545c67966370907b20d3
ibtesamrauf <irkibby@gmail.com>
2017-12-23 15:30:48 +0500

    first push

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Handle an authentication attempt.
+     *
+     * @return Response
+     */
+    public function authenticate() {
+        if ( Auth::attempt( ['email' => $email, 'password' => $password, 'verified' => 1] ) ) {
+            // Authentication passed...
+            return redirect()->intended( '/' );
+        }
+    }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        // var_dump($request);
+        // die;
+        return [
+            'email' => $request->{$this->username()},
+            'password' => $request->password,
+            'verified' => '1',
+        ];
+    }
+
+
+}
